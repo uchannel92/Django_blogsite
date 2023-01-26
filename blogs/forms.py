bc404f486b507d78a1407efb810aa0ed470f3c40
Uchannel92 <uchenna_attah@hotmail.com>
2023-01-26 21:14:03 +0000

    updated styling on the blogs page, register and logout pages.

--- blogs/forms.py
+++ blogs/forms.py
@@ -6,5 +6,5 @@ class BlogForm(forms.ModelForm):
 	class Meta:
 		model = BlogPost
 		fields = ['title', 'text']
-		labels = {'title': '','text': '',}
+		labels = {'title': 'Title','text': 'Text',}
 		widgets = {'text': forms.Textarea(attrs={'cols': 80})}
